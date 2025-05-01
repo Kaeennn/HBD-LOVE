f9400bba011bf746a20e688ffb88747bcfb1d214
Kaeennn <poucheaters@gmail.com>
2025-05-02 00:24:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-const messages = [
-  "You're my favorite person ❤️",
-  "You light up my life 🌟",
-  "Every moment with you is magic ✨",
-  "I love you more than words 💕",
-  "You're my forever 🎀"
-];
-
-const leftImages = [
- "C:\Users\User\Desktop\HBD LOVE\images\1.jpg",
-  "C:\Users\User\Desktop\HBD LOVE\images\2.jpg",
-  "C:\Users\User\Desktop\HBD LOVE\images\3.jpg"
-];
-
-const rightImages = [
-  "C:\Users\User\Desktop\HBD LOVE\images\4.jpg",
-  "C:\Users\User\Desktop\HBD LOVE\images\5.jpg",
-  "C:\Users\User\Desktop\HBD LOVE\images\6.jpg"
-];
-
-document.getElementById('revealBtn').addEventListener('click', () => {
-  document.getElementById('revealBtn').classList.add('hidden');
-  document.getElementById('surpriseContent').classList.remove('hidden');
-});
-
-document.getElementById('newSurpriseBtn').addEventListener('click', () => {
-  const msg = messages[Math.floor(Math.random() * messages.length)];
-  const left = leftImages[Math.floor(Math.random() * leftImages.length)];
-  const right = rightImages[Math.floor(Math.random() * rightImages.length)];
-
-  document.getElementById('randomMessage').textContent = msg;
-  document.getElementById('leftImage').src = left;
-  document.getElementById('rightImage').src = right;
-});
+function showMessage() {
+  document.getElementById("message").innerText = "I love you so much! 💖";
+}
